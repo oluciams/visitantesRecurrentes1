d136315333a5a8abe6d5522d31ded2f299f6b862
olga <oluciams84@gmail.com>
2021-07-14 20:54:50 -0500

    Creacion de registro funcionando a medias

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,24 +1,48 @@
 const router = require('express').Router()
 
+
+
 const Visitor = require('../models/visitor.model')
 
+
+const isNameCreated = (req, res, next) => {
+
+    
+    next()
+}
+
  
-router.get('/', async(req, res)=>{
-    const visitor = new Visitor({
-        name: req.query.name || "Anónimo"               
-    })
-    await visitor.save()
+router.get('/', (req, res)=>{
+    
+    Visitor.findOne({ "name": req.query.name }, function(err, visitor) {
+        if (err) return console.error(err);
+        if(visitor){
+            visitor.count = visitor.count + 1
+            visitor.save(function (err){
+            if(err) return console.error(err)
+            })
+            console.log(`coincidí ${visitor}`)
+
+        }else {
+            const visitor = new Visitor({
+                name: req.query.name || "Anónimo"               
+            })
+            visitor.save()
+            console.log(`se creó uno nuevo ${visitor}`);
+        }
+    });
 
     Visitor.find(function(err, visitors) {
         if (err) return console.error(err);
         console.log(visitors);
         res.render('index', {visitors: visitors})
-    });    
+    }); 
+   
  })
 
 
 
 
-
+  
 
 module.exports = router
